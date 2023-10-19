1744bcc7fc980eb65041999c2bff95a7b4359e74
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 15:15:12 -0400

    Create InteractionCheckerTestDefs

---
+++ src/test/java/definitions/InteractionCheckerTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class InteractionCheckerTestDefs extends TestSetupDefs{
+    @Given("I have a predefined list of medication interactions")
+    public void iHaveAPredefinedListOfMedicationInteractions() {
+    }
+}

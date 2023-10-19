eb629edb54968401a84e6d4f27885d9909981f17
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 18:00:59 -0400

    Add Cucumber tests for public register endpoint

--- src/test/java/definitions/AuthenticationTestDefs.java
+++ src/test/java/definitions/AuthenticationTestDefs.java
@@ -4,9 +4,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -49,20 +52,42 @@ public class AuthenticationTestDefs extends TestSetupDefs {
     }
 
     // Scenario: Successful registration
-    @Given("I am on the registration page")
-    public void iAmOnTheRegistrationPage() {
-
+    @Given("I am on the registration page {string}")
+    public void iAmOnTheRegistrationPage(String register) {
+        logger.info("Scenario: Successful registration - Step: I am on the registration page");
+        response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post(BASE_URL + port + register)
+                .then()
+                .extract()
+                .response();
+        int statusCode = response.statusCode();
+        assert statusCode == 200 : "Expected status code to be 200, but it was: " + statusCode;
     }
 
 
-    @When("I enter a valid email address and a strong password")
-    public void iEnterAValidEmailAddressAndAStrongPassword() {
-
+    @When("I enter a valid email address {string} and a strong password {string}")
+    public void iEnterAValidEmailAddressAndAStrongPassword(String emailAddress, String password) {
+        logger.info("Scenario: Successful registration - Step: I enter a valid email address and a strong password");
+        RestAssured.baseURI = BASE_URL;
+        RequestSpecification request = RestAssured.given();
+        try {
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("emailAddress", emailAddress);
+            requestBody.put("password", password);
+            request.header("Content-Type", "application/json");
+            response = request.body(requestBody.toString()).post(BASE_URL + port + registerEndpoint);
+        } catch (Exception e) {
+            logger.warning("Exception occurred: " + e.getMessage());
+            Assert.fail("Failed Exception");
+        }
     }
 
     @Then("I should be successfully registered")
     public void iShouldBeSuccessfullyRegistered() {
-
+        logger.info("Scenario: Successful registration - Step: I should be successfully registered");
+        Assert.assertEquals(201,response.getStatusCode());
     }
 
     // Scenario: User able to login and receive jwt token
--- src/test/resources/features/AuthenticationTest.feature
+++ src/test/resources/features/AuthenticationTest.feature
@@ -6,8 +6,8 @@ Feature: Authentication
     Then Hello is shown
 
   Scenario: Successful registration
-    Given I am on the registration page
-    When I enter a valid email address and a strong password
+    Given I am on the registration page "/auth/users/register/"
+    When I enter a valid email address "test@email.com" and a strong password "password12345"
     Then I should be successfully registered
 
   Scenario: User able to login and receive jwt token

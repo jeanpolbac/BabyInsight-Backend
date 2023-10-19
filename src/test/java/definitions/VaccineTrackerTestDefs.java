d75d2cb444b424eae14e767e1c4a099909a91380
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 15:04:04 -0400

    Create VaccineTrackerTestDefs

---
+++ src/test/java/definitions/VaccineTrackerTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class VaccineTrackerTestDefs extends TestSetupDefs{
+    @Given("the system has a list of standard vaccines for newborns in the database")
+    public void theSystemHasAListOfStandardVaccinesForNewbornsInTheDatabase() {
+    }
+}

9dc8850ea353227770f6a4dd0327bb5f927e1463
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 13:07:32 -0400

    Extend AuthenticationTestDefs with TestSetupDefs for configuration

--- src/test/java/definitions/AuthenticationTestDefs.java
+++ src/test/java/definitions/AuthenticationTestDefs.java
@@ -2,7 +2,7 @@ package definitions;
 
 import io.cucumber.java.en.Given;
 
-public class AuthenticationTestDefs {
+public class AuthenticationTestDefs extends TestSetupDefs {
     @Given("a valid public endpoint")
     public void aValidPublicEndpoint() {
     }

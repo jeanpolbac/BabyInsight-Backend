f1d24f909a4dfcc66e3579566acc40545842a218
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 15:35:21 -0400

    Add common endpoints to TestSetupDefs class for Cucumber tests

--- src/test/java/definitions/TestSetupDefs.java
+++ src/test/java/definitions/TestSetupDefs.java
@@ -9,6 +9,24 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BabyInsightBackendApplication.class)
 public class TestSetupDefs {
 
+    // Base URL for testing
+    public static final String BASE_URL = "http://localhost:";
+
+    // Authentication Endpoints
+    public static final String helloEndpoint = "/auth/users/hello/";
+    public static final String registerEndpoint = "/auth/users/register/";
+    public static final String loginEndpoint= "/auth/users/login/";
+
+    // Interaction Checker Endpoints
+    public static final String interactionEndpoint= "/api/interactions?med1={medicationID1}&med2={medicationID2}/";
+
+    // Medication Log Endpoints
+    public static final String allMedicationEndpoint= "/api/medications/{childID}/";
+
+
+    // Content-Type Json
+    public static final String TypeJson = "application/json";
+
     @LocalServerPort
     public String port;
 

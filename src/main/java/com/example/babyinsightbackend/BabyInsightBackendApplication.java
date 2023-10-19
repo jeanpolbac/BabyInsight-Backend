45fa707bcf1e0d1d209f18d22ec6994f402ea4a9
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 12:03:28 -0400

    first commit

---
+++ src/main/java/com/example/babyinsightbackend/BabyInsightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.babyinsightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyInsightBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyInsightBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/babyinsightbackend/BabyInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babyinsightbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyInsightBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6b3c83a1e35d54a25017214eb42309e91649088c
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 22:30:38 -0400

    Create ChildManagementTestDefs

---
+++ src/test/java/definitions/ChildManagementTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class ChildManagementTestDefs extends TestSetupDefs{
+    @Given("the parent is logged in with credentials {string} and password {string}")
+    public void theParentIsLoggedInWithCredentialsAndPassword(String arg0, String arg1) {
+    }
+}

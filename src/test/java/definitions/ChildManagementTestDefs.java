49d153f2f242bc641c44be519e89eec4f2bccbfd
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-19 22:47:24 -0400

    Add Cucumber tests for adding child to parent profile

--- src/test/java/definitions/ChildManagementTestDefs.java
+++ src/test/java/definitions/ChildManagementTestDefs.java
@@ -4,7 +4,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.Assert;
 
 import java.util.logging.Logger;
 
@@ -14,18 +19,44 @@ public class ChildManagementTestDefs extends TestSetupDefs{
 
     private static Response response;
 
+    // Scenario: Parent able to add a child to their profile for tracking vaccines and medication
     @Given("the parent is logged in with credentials {string} and password {string}")
     public void theParentIsLoggedInWithCredentialsAndPassword(String emailAddress, String password) {
+        logger.info("Scenario: Parent able to add a child to their profile - Step: The parent is logged in");
+        try {
+            RequestSpecification request = RestAssured.given();
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("emailAddress", emailAddress);
+            jsonObject.put("password", password);
+
+            response = request.contentType(ContentType.JSON).body(jsonObject.toString()).post(BASE_URL + port + loginEndpoint);
+        } catch (Exception e) {
+            logger.warning("Exception occurred: " + e.getMessage());
+            Assert.fail("Test failed due to an exception");
+        }
     }
 
     @When("the parent adds a child with name {string} and date of birth {string}")
     public void theParentAddsAChildWithNameAndDateOfBirth(String childName, String dob) {
-        
+        logger.info("Scenario: Parent able to add a child to their profile - Step: The parent adds a child");
+        try {
+            RequestSpecification request = RestAssured.given();
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("childName", childName);
+            jsonObject.put("dateOfBirth", dob);
+
+            response = request.contentType(ContentType.JSON).body(jsonObject.toString()).post(BASE_URL + port + childEndpoint);
+        } catch (Exception e) {
+            logger.warning("Exception occurred: " + e.getMessage());
+            Assert.fail("Test failed due to an exception");
+        }
     }
 
     @Then("the child should be successfully added to the parent's profile")
     public void theChildShouldBeSuccessfullyAddedToTheParentSProfile() {
-        
+        logger.info("Scenario: Parent able to add a child to their profile - Step: The child is successfully added");
+        Assert.assertEquals(201, response.getStatusCode());
+        Assert.assertNotNull(response.jsonPath().getString("childId"));
     }
 
     @When("the parent views the list of their children")

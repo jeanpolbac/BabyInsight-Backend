f70e3e35579936cb38db4dca402ea08c614fe565
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-21 18:31:27 -0400

    Document methods in ChildManagementTestDefs for clarity

--- src/test/java/definitions/ChildManagementTestDefs.java
+++ src/test/java/definitions/ChildManagementTestDefs.java
@@ -15,6 +15,10 @@ import org.junit.Assert;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * The ChildManagementTestDefs class contains step definitions for Cucumber scenarios
+ * related to managing child profiles by a parent.
+ */
 public class ChildManagementTestDefs extends TestSetupDefs {
 
     private static final Logger logger = Logger.getLogger(ChildManagementTestDefs.class.getName());
@@ -22,6 +26,12 @@ public class ChildManagementTestDefs extends TestSetupDefs {
     private static Response response;
 
 
+    /**
+     * Sends a POST request to add a new child to the parent's profile.
+     *
+     * @param childName the name of the child
+     * @param dob the date of birth of the child
+     */
     // Scenario: Parent able to add a child to their profile for tracking vaccines and medication
     @When("the parent adds a child with name {string} and date of birth {string}")
     public void theParentAddsAChildWithNameAndDateOfBirth(String childName, String dob) {
@@ -39,6 +49,9 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         }
     }
 
+    /**
+     * Validates that the child was successfully added to the parent's profile.
+     */
     @Then("the child should be successfully added to the parent's profile")
     public void theChildShouldBeSuccessfullyAddedToTheParentSProfile() {
         logger.info("Scenario: Parent able to add a child to their profile - Step: The child is successfully added");
@@ -46,6 +59,10 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         Assert.assertNotNull(response.jsonPath().getString("childId"));
     }
 
+
+    /**
+     * Sends a GET request to view the list of children associated with the parent's profile.
+     */
     // Scenario: Parent able to view a list of their children
     @When("the parent views the list of their children")
     public void theParentViewsTheListOfTheirChildren() {
@@ -59,6 +76,11 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         }
     }
 
+    /**
+     * Validates that the specified child's name is included in the list of children.
+     *
+     * @param childName the name of the child
+     */
     @Then("the list should include {string}")
     public void theListShouldInclude(String childName) {
         logger.info("Scenario: Parent able to view a list of their children - Step: The list should include child");
@@ -67,6 +89,12 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         Assert.assertTrue(childNames.contains(childName));
     }
 
+
+    /**
+     * Verifies that the specified child exists in the parent's profile.
+     *
+     * @param childName the name of the child
+     */
     // Scenario: Parent able to edit a child's details
     @Given("the child {string} exists in the parent's profile")
     public void theChildExistsInTheParentsProfile(String childName) {
@@ -80,7 +108,11 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         Assert.assertTrue("Child does not exist in the parent's profile", childNames.contains(childName));
     }
 
-
+    /**
+     * Sends a PUT request to edit the name of the child in the parent's profile.
+     *
+     * @param newName the new name for the child
+     */
     @When("the parent edits the child's name to {string}")
     public void theParentEditsTheChildSNameTo(String newName) {
         logger.info("Scenario: Parent able to edit a child's details - Step: The parent edits the child's name");
@@ -96,6 +128,11 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         }
     }
 
+    /**
+     * Validates that the child's name was successfully updated in the parent's profile.
+     *
+     * @param newName the new name for the child
+     */
     @Then("the child's name should be updated to {string}")
     public void theChildSNameShouldBeUpdatedTo(String newName) {
         logger.info("Scenario: Parent able to edit a child's details - Step: The child's name is updated");
@@ -103,6 +140,12 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         Assert.assertEquals(newName, response.jsonPath().getString("newName"));
     }
 
+
+    /**
+     * Sends a GET request to view the profile of the specified child.
+     *
+     * @param childName the name of the child
+     */
     // Scenario: Parent able to view a child's profile
     @When("the parent views the profile of {string}")
     public void theParentViewsTheProfileOf(String childName) {
@@ -117,6 +160,12 @@ public class ChildManagementTestDefs extends TestSetupDefs {
         }
     }
 
+    /**
+     * Validates that the child's profile displays the correct name and date of birth.
+     *
+     * @param childName the name of the child
+     * @param dob the date of birth of the child
+     */
     @Then("the profile should display the name {string} and date of birth {string}")
     public void theProfileShouldDisplayTheNameAndDateOfBirth(String childName, String dob) {
         logger.info("Scenario: Parent able to view a child's profile - Step: The profile displays the correct details");

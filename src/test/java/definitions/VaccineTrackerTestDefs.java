e31508e03cf81c159e6569fd2822fde1d87d9d9a
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-21 18:37:00 -0400

    Document methods in VaccineTrackerTestDefs for clarity

--- src/test/java/definitions/VaccineTrackerTestDefs.java
+++ src/test/java/definitions/VaccineTrackerTestDefs.java
@@ -9,6 +9,10 @@ import io.restassured.specification.RequestSpecification;
 import static org.hamcrest.Matchers.*;
 import java.util.logging.Logger;
 
+/**
+ * The VaccineTrackerTestDefs class contains step definitions for Cucumber scenarios
+ * related to vaccine tracking and retrieval.
+ */
 public class VaccineTrackerTestDefs extends TestSetupDefs{
 
     private static final Logger logger = Logger.getLogger(VaccineTrackerTestDefs.class.getName());
@@ -16,6 +20,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
     private static RequestSpecification request;
 
 
+    /**
+     * Ensures that the system has a pre-loaded list of standard vaccines for newborns.
+     */
     // Scenario: Retrieve pre-loaded list of vaccines
     @Given("the system has a list of standard vaccines for newborns in the database")
     public void theSystemHasAListOfStandardVaccinesForNewbornsInTheDatabase() {
@@ -23,6 +30,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         request = RestAssured.given();
     }
 
+    /**
+     * Sends a request to fetch the list of vaccines.
+     */
     @When("a request is made to fetch the list of vaccines")
     public void aRequestIsMadeToFetchTheListOfVaccines() {
         logger.info("Scenario: Retrieve pre-loaded list of vaccines from database - Step: A request is made to fetch the list of vaccines");
@@ -34,6 +44,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         }
     }
 
+    /**
+     * Validates the response to ensure the pre-loaded list of standard vaccines is returned.
+     */
     @Then("the system should return the pre-loaded list of standard vaccines")
     public void theSystemShouldReturnThePreLoadedListOfStandardVaccines() {
         logger.info("Scenario: Retrieve pre-loaded list of vaccines from database - Step: The system should return the pre-loaded list of standard vaccines");
@@ -41,6 +54,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
     }
 
 
+    /**
+     * Ensures that the system has a record of vaccines administered to a specified child.
+     */
     // Scenario: Retrieve list of administered vaccines for a child
     @Given("the system has a record of vaccines administered to a child")
     public void theSystemHasARecordOfVaccinesAdministeredToAChild() {
@@ -48,6 +64,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         request = RestAssured.given();
     }
 
+    /**
+     * Sends a request to fetch the list of administered vaccines for the specified child.
+     */
     @When("a request is made to fetch the list of administered vaccines for the child")
     public void aRequestIsMadeToFetchTheListOfAdministeredVaccinesForTheChild() {
         logger.info("Scenario: Retrieve list of administered vaccines for a child - Step: A request is made to fetch the list of administered vaccines for the child");
@@ -59,6 +78,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         }
     }
 
+    /**
+     * Validates the response to ensure the list of administered vaccines is returned.
+     */
     @Then("the system should return the list of vaccines administered to the child")
     public void theSystemShouldReturnTheListOfVaccinesAdministeredToTheChild() {
         logger.info("Scenario: Retrieve list of administered vaccines for a child - Step: The system should return the list of vaccines administered to the child");
@@ -66,6 +88,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
     }
 
 
+    /**
+     * Ensures that the system has a record of vaccines to be administered to a specified child.
+     */
     // Scenario: Retrieve list of remaining vaccines for a child
     @Given("the system has a record of vaccines to be administered to a child")
     public void theSystemHasARecordOfVaccinesToBeAdministeredToAChild() {
@@ -73,6 +98,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         request = RestAssured.given();
     }
 
+    /**
+     * Sends a request to fetch the list of remaining vaccines for the specified child.
+     */
     @When("a request is made to fetch the list of remaining vaccines for the child")
     public void aRequestIsMadeToFetchTheListOfRemainingVaccinesForTheChild() {
         logger.info("Scenario: Retrieve list of remaining vaccines for a child - Step: A request is made to fetch the list of remaining vaccines for the child");
@@ -84,6 +112,9 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         }
     }
 
+    /**
+     * Validates the response to ensure the list of remaining vaccines is returned.
+     */
     @Then("the system should return the list of vaccines yet to be administered to the child")
     public void theSystemShouldReturnTheListOfVaccinesYetToBeAdministeredToTheChild() {
         logger.info("Scenario: Retrieve list of remaining vaccines for a child - Step: The system should return the list of vaccines yet to be administered to the child");
@@ -91,6 +122,12 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
     }
 
 
+    /**
+     * Triggers the specified action for a specified vaccine on a specified date.
+     * @param action The action to be triggered.
+     * @param vaccine The vaccine the action is being triggered for.
+     * @param date The date the action is triggered.
+     */
     // Scenario: Update a vaccine as administered in the database
     @When("the {string} action is triggered for {string} vaccine with a date of {string}")
     public void theActionIsTriggeredForVaccineWithADateOf(String action, String vaccine, String date) {
@@ -108,6 +145,12 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
         }
     }
 
+    /**
+     * Validates the vaccine status and administered date are updated as expected.
+     * @param vaccineName The name of the vaccine.
+     * @param status The updated status of the vaccine.
+     * @param date The date the vaccine was administered.
+     */
     @Then("{string} status should be updated to {string} with the date {string}")
     public void statusShouldBeUpdatedToWithTheDate(String vaccineName, String status, String date) {
         logger.info("Scenario: Update a vaccine as administered in the database - Step: That vaccine's status should be updated to " + status + " with the date " + date);

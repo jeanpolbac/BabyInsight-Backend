6e52e321265b5f95e91907f866cbbab8a7dd3861
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 00:08:10 -0400

    Create initial JwtRequestFilter

---
+++ src/main/java/com/example/babyinsightbackend/security/JwtRequestFilter.java
@@ -0,0 +1,9 @@
+package com.example.babyinsightbackend.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+}

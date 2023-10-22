1216defb4d0a74eb0d27d619c67492da86708ce1
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 17:41:47 -0400

    Removed createHeader and getJWTToken from TestSetupDefs as it was causing problems.

--- src/test/java/definitions/TestSetupDefs.java
+++ src/test/java/definitions/TestSetupDefs.java
@@ -2,15 +2,8 @@ package definitions;
 
 import com.example.babyinsightbackend.BabyInsightBackendApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import org.springframework.http.*;
 
 import java.util.logging.Logger;
 
@@ -31,8 +24,8 @@ public class TestSetupDefs {
 
     // Authentication Endpoints
     public static final String helloEndpoint = "/hello/";
-    public static final String registerEndpoint = "/auth/parents/register/";
-    public static final String loginEndpoint = "/auth/parents/login/";
+    public static final String registerEndpoint = "/auth/users/register/";
+    public static final String loginEndpoint = "/auth/users/login/";
 
 
     // Child Management Endpoints
@@ -66,51 +59,5 @@ public class TestSetupDefs {
     public String port;
 
 
-    /**
-     * Creates headers for authenticated requests.
-     * @return a HttpHeaders object with an Authorization header containing a JWT token and a Content-Type header set to application/json
-     * @throws JSONException if there's an error while creating the JSON request body
-     */
-    protected HttpHeaders createAuthHeaders() throws JSONException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + getJWTToken());
-        headers.add("Content-Type", TypeJson);
-        return headers;
-    }
 
-    /**
-     * Retrieves a JWT token for authentication.
-     * @return a JWT token as a String
-     * @throws JSONException if there's an error while creating the JSON request body
-     */
-    protected String getJWTToken() throws JSONException {
-        // Set the base URI and create a request
-        RestAssured.baseURI = BASE_URL + port;
-        RequestSpecification request = RestAssured.given();
-
-        // Set the content-type header to indicate JSON data
-        request.header("Content-Type", TypeJson);
-
-        // Create a JSON request body with user email and password
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("emailAddress", "test@email.com");
-        requestBody.put("password", "password12345");
-
-        // Send a POST request to the authentication endpoint
-        Response response = request.body(requestBody.toString()).post(BASE_URL + port + loginEndpoint);
-
-        // Debugging: Log the response content and HTTP status
-        String responseBody = response.getBody().asString();
-        logger.info("Response Body: " + responseBody);
-        int statusCode = response.getStatusCode();
-        logger.info("HTTP Status Code: " + statusCode);
-
-        // Extract and return the JWT token from the authentication response
-        // Check for the presence of JWT and extract
-        if (response.jsonPath().get("jwt") != null) {
-            return response.jsonPath().getString("jwt");
-        } else {
-            throw new RuntimeException("JWT not found in the response");
-        }
-    }
 }

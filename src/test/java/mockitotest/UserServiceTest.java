912d0ff354ca7028fd1824c0373d619080fa0d08
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 13:01:27 -0400

    Create initial Mockito UserServiceTest

---
+++ src/test/java/mockitotest/UserServiceTest.java
@@ -0,0 +1,9 @@
+package mockitotest;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+}

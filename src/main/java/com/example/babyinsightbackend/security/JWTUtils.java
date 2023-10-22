6c5d83cb0d6cfc9767bb15e48093e2bf1c51c41a
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 00:04:29 -0400

    Configure JWTUtils class to handle JWT generation, parsing, and validation

--- src/main/java/com/example/babyinsightbackend/security/JWTUtils.java
+++ src/main/java/com/example/babyinsightbackend/security/JWTUtils.java
@@ -1,7 +1,82 @@
 package com.example.babyinsightbackend.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Utility service for handling JSON Web Tokens (JWT).
+ * This service provides methods to generate, parse, and validate JWTs.
+ */
 @Service
 public class JWTUtils {
+
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private long jwtExpMS;
+
+
+    /**
+     * Generates a JWT token for a given user.
+     *
+     * @param myUserDetails The user details for whom the JWT is generated.
+     * @return The JWT token string.
+     */
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()
+                .setSubject(myUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpMS))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+
+    /**
+     * Extracts the username from the given JWT token.
+     *
+     * @param token The JWT token to parse.
+     * @return The username extracted from the token.
+     */
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+    /**
+     * Validates the provided JWT token.
+     *
+     * @param token The JWT token to validate.
+     * @return true if the token is valid; false otherwise.
+     */
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            logger.log(Level.SEVERE, "Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.log(Level.SEVERE, "Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
 }

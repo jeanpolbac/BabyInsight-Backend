0124f26bdeb4509a3ed2ad107977bf849720ada3
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 14:32:20 -0400

    Added loginUser method for user authentication in UserService

--- src/main/java/com/example/babyinsightbackend/service/UserService.java
+++ src/main/java/com/example/babyinsightbackend/service/UserService.java
@@ -2,10 +2,21 @@ package com.example.babyinsightbackend.service;
 
 import com.example.babyinsightbackend.exception.InformationExistException;
 import com.example.babyinsightbackend.models.User;
+import com.example.babyinsightbackend.models.request.LoginRequest;
 import com.example.babyinsightbackend.repository.UserRepository;
 
+import com.example.babyinsightbackend.security.JWTUtils;
+import com.example.babyinsightbackend.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 /**
@@ -16,14 +27,25 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+
+    private AuthenticationManager authenticationManager;
+
     /**
      * Constructs a new UserService object.
      *
-     * @param userRepository The repository used to interact with User data in the database.
+     * @param userRepository        The repository used to interact with User data in the database.
+     * @param passwordEncoder
+     * @param jwtUtils
+     * @param authenticationManager
      */
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
     /**
@@ -41,6 +63,26 @@ public class UserService {
             throw new InformationExistException("user email address " + userObject.getEmailAddress() + " already exists");
         }
     }
+
+
+    /**
+     * Authenticates a user based on the provided login credentials and generates a JWT token upon successful authentication.
+     *
+     * @param loginRequest The login request containing the user's email address and password.
+     * @return An Optional containing the JWT token if authentication is successful, or an empty Optional if authentication fails.
+     */
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
 
     /**
      * Fetches a User based on the provided email address.

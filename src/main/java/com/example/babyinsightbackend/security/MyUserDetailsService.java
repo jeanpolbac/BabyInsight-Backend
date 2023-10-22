26c1b3d3618802ce639f76398f1ec9cc498bab67
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-21 23:34:09 -0400

    Create initial MyUserDetailsService

---
+++ src/main/java/com/example/babyinsightbackend/security/MyUserDetailsService.java
@@ -0,0 +1,8 @@
+package com.example.babyinsightbackend.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+}

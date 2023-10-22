6ec7b2833d6091ba2618259871f5ba4895b81e63
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-21 22:35:48 -0400

    Define attributes and annotations for User model

--- src/main/java/com/example/babyinsightbackend/models/User.java
+++ src/main/java/com/example/babyinsightbackend/models/User.java
@@ -1,4 +1,25 @@
 package com.example.babyinsightbackend.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String userName;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String emailAddress;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
+

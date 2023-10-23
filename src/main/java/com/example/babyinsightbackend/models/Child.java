2bd504be2d560f8bbf1e9269927031b11f143514
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 21:41:42 -0400

    Create initial Child model

---
+++ src/main/java/com/example/babyinsightbackend/models/Child.java
@@ -0,0 +1,11 @@
+package com.example.babyinsightbackend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "children")
+public class Child {
+
+
+}

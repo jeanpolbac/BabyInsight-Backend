dd56668044020766e3f247b3e63c517cf6b88d0c
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 22:30:29 -0400

    Create initial ChildController

---
+++ src/main/java/com/example/babyinsightbackend/controller/ChildController.java
@@ -0,0 +1,9 @@
+package com.example.babyinsightbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/parents/{parentID}")
+public class ChildController {
+}

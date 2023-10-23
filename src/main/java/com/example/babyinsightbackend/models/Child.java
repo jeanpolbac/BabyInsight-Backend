580933b1d1ddd531080fc7035b7f0ce1aff277dd
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-22 21:46:05 -0400

    Add id, name, and dateOfBirth fields to Child model

--- src/main/java/com/example/babyinsightbackend/models/Child.java
+++ src/main/java/com/example/babyinsightbackend/models/Child.java
@@ -1,11 +1,22 @@
 package com.example.babyinsightbackend.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "children")
 public class Child {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
 
 
 }

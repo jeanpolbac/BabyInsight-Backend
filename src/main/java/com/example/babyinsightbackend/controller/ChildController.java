0b9d7a7b829c94e95f6a6df704ce651089945b89
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-23 19:27:56 -0400

    Implement getAllChildrenByParent method in ChildController

--- src/main/java/com/example/babyinsightbackend/controller/ChildController.java
+++ src/main/java/com/example/babyinsightbackend/controller/ChildController.java
@@ -3,9 +3,11 @@ package com.example.babyinsightbackend.controller;
 import com.example.babyinsightbackend.models.Child;
 import com.example.babyinsightbackend.service.ChildService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 @RequestMapping("/api/users/{userId}")
 public class ChildController {
 
+    static HashMap<String, Object> message = new HashMap<>();
 
     @Autowired
     private ChildService childService;
@@ -36,45 +39,52 @@ public class ChildController {
      * @return A list of children.
      */
     @GetMapping("/children")
-    public List<Child> getAllChildrenByParent(@PathVariable Long userId) {
-        return childService.getAllChildrenByParentId(userId);
+    public ResponseEntity<?> getAllChildrenByParent(@PathVariable Long userId) {
+        List<Child> children = childService.getAllChildrenByParentId(userId);
+        if (children.isEmpty()) {
+            message.put("message", "cannot find any children");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "success");
+            message.put("data", children);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
     }
 
     /**
      * Retrieve a specific child for a specific parent.
      *
-     * @param userId  The ID of the parent.
-     * @param childID The ID of the child.
+     * @param childId The ID of the child.
      * @return The child details.
      */
-    @GetMapping("/children/{childID}")
-    public Optional<Child> getChildByParent(@PathVariable Long userId, @PathVariable Long childID) {
-        return childService.getChildById(userId, childID);
+    @GetMapping("/children/{childId}")
+    public Optional<Child> getChildByParent( @PathVariable Long childId) {
+        return childService.getChildById(childId);
     }
 
     /**
      * Update child details for a specific parent.
      *
      * @param userId The ID of the parent.
-     * @param childID The ID of the child.
+     * @param childId The ID of the child.
      * @param child The child details to be updated.
      * @return The updated child.
      */
-    @PutMapping("/children/{childID}")
-    public Child updateChild(@PathVariable Long userId, @PathVariable Long childID, @RequestBody Child child) {
-        return childService.updateChild(userId, childID, child);
+    @PutMapping("/children/{childId}")
+    public Child updateChild(@PathVariable Long userId, @PathVariable Long childId, @RequestBody Child child) {
+        return childService.updateChild(userId, childId, child);
     }
 
     /**
      * Delete a child for a specific parent.
      *
      * @param userId The ID of the parent.
-     * @param childID The ID of the child.
+     * @param childId The ID of the child.
      * @return A response indicating success or failure.
      */
-    @DeleteMapping("/children/{childID}")
-    public ResponseEntity<?> deleteChild(@PathVariable Long userId, @PathVariable Long childID) {
-        childService.deleteChild(userId, childID);
+    @DeleteMapping("/children/{childId}")
+    public ResponseEntity<?> deleteChild(@PathVariable Long userId, @PathVariable Long childId) {
+        childService.deleteChild(userId, childId);
         return ResponseEntity.ok().build();
     }
 

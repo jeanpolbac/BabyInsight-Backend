0aabb32977d49d381f55434f0773dc76f1bd2740
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-23 21:47:27 -0400

    Add findByName to ChildRepository

--- src/main/java/com/example/babyinsightbackend/repository/ChildRepository.java
+++ src/main/java/com/example/babyinsightbackend/repository/ChildRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
 
     List<Child> findByUserId(Long userId);
+
+    Optional<Child> findByName(String name);
+
 
 }

3009e6367467a821ac3d9f17633cc7611326d230
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-23 21:50:21 -0400

    Create initial Vaccine model

---
+++ src/main/java/com/example/babyinsightbackend/models/Vaccine.java
@@ -0,0 +1,10 @@
+package com.example.babyinsightbackend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vaccines")
+public class Vaccine {
+
+}

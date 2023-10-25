5f08fee27829ac8f89109446d30f03e8f1836e5b
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 00:21:46 -0400

    Refactor tests in VaccineTrackerTestDefs

--- src/test/java/definitions/VaccineTrackerTestDefs.java
+++ src/test/java/definitions/VaccineTrackerTestDefs.java
@@ -1,13 +1,28 @@
 package definitions;
 
+import com.example.babyinsightbackend.models.Child;
+import com.example.babyinsightbackend.models.User;
+import com.example.babyinsightbackend.models.Vaccine;
+import com.example.babyinsightbackend.repository.ChildRepository;
+import com.example.babyinsightbackend.repository.UserRepository;
+import com.example.babyinsightbackend.repository.VaccineRepository;
+import com.example.babyinsightbackend.service.ChildService;
+import com.example.babyinsightbackend.service.VaccineService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import static org.hamcrest.Matchers.*;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * The VaccineTrackerTestDefs class contains step definitions for Cucumber scenarios
@@ -17,150 +32,156 @@ public class VaccineTrackerTestDefs extends TestSetupDefs{
 
     private static final Logger logger = Logger.getLogger(VaccineTrackerTestDefs.class.getName());
     private static Response response;
-    private static RequestSpecification request;
+
+    @Autowired
+    private VaccineService vaccineService;
+    @Autowired
+    private ChildService childService;
+    @Autowired
+    private VaccineRepository vaccineRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChildRepository childRepository;
+    private List<Vaccine> administeredVaccines;
+
+    private List<Vaccine> fetchedAdministeredVaccines;
+
+    private Vaccine newVaccine;
+    private Vaccine createdVaccine;
+    private List<Vaccine> existingVaccines;
+    private List<Vaccine> retrieveVaccineList;
+    private Optional<Vaccine> retrievedVaccine;
+
+    private static Child child;
+    private Long vaccineId;
 
 
-    /**
-     * Ensures that the system has a pre-loaded list of standard vaccines for newborns.
-     */
-    // Scenario: Retrieve pre-loaded list of vaccines
-    @Given("the system has a list of standard vaccines for newborns in the database")
-    public void theSystemHasAListOfStandardVaccinesForNewbornsInTheDatabase() {
-        logger.info("Scenario: Retrieve pre-loaded list of vaccines from database - Step: The system has a list of standard vaccines for newborns in the database");
-        request = RestAssured.given();
+
+
+    @Given("a new vaccine with name {string}")
+    public void aNewVaccineWithName(String vaccineName) {
+        newVaccine = new Vaccine();
+        newVaccine.setName(vaccineName);
     }
 
-    /**
-     * Sends a request to fetch the list of vaccines.
-     */
-    @When("a request is made to fetch the list of vaccines")
-    public void aRequestIsMadeToFetchTheListOfVaccines() {
-        logger.info("Scenario: Retrieve pre-loaded list of vaccines from database - Step: A request is made to fetch the list of vaccines");
-        try {
-            response = request.get(allVaccinesEndpoint);
-            logger.info("Received response with status code: " + response.statusCode());
-        } catch (Exception e) {
-            logger.severe("An error occurred while fetching the list of vaccines: " + e.getMessage());
-        }
+    @When("the vaccine is added to the system")
+    public void theVaccineIsAddedToTheSystem() {
+        createdVaccine = vaccineService.addVaccine(newVaccine, child);
     }
 
-    /**
-     * Validates the response to ensure the pre-loaded list of standard vaccines is returned.
-     */
-    @Then("the system should return the pre-loaded list of standard vaccines")
-    public void theSystemShouldReturnThePreLoadedListOfStandardVaccines() {
-        logger.info("Scenario: Retrieve pre-loaded list of vaccines from database - Step: The system should return the pre-loaded list of standard vaccines");
-        response.then().statusCode(200);
+    @Then("the system should return the created vaccine")
+    public void theSystemShouldReturnTheCreatedVaccine() {
+        Assert.assertNotNull(createdVaccine);
+        assertEquals(newVaccine.getName(), createdVaccine.getName());
     }
 
 
-    /**
-     * Ensures that the system has a record of vaccines administered to a specified child.
-     */
-    // Scenario: Retrieve list of administered vaccines for a child
+    @Given("there are existing vaccines in the system for a child at {int} months")
+    public void thereAreExistingVaccinesInTheSystem(int childAgeInMonths) {
+        existingVaccines = vaccineService.getRequiredVaccinesForChildByAge(childAgeInMonths);
+        logger.info("Existing vaccines: " + existingVaccines);
+    }
+
+    @When("I retrieve all vaccines")
+    public void iRetrieveAllVaccines() {
+        retrieveVaccineList = vaccineService.getAllVaccines();
+        logger.info("Retrieved vaccines: " + retrieveVaccineList);
+
+    }
+
+    @Then("I should get a list of vaccines")
+    public void iShouldGetAListOfVaccines() {
+        Assert.assertNotNull(retrieveVaccineList);
+        logger.info("Retrieved vaccines: " + retrieveVaccineList);
+    }
+
+
+    @Given("there is a vaccine with ID {int} in the system")
+    public void thereIsAVaccineWithIDInTheSystem(int vaccineId) {
+        Vaccine vaccine = new Vaccine();
+        vaccine.setVaccineId((long) vaccineId);
+        vaccineService.addVaccine(vaccine, child);
+        this.vaccineId = (long) vaccineId;
+    }
+
+    @When("I retrieve the vaccine with ID {int}")
+    public void iRetrieveTheVaccineWithID(int vaccineId) {
+        retrievedVaccine = vaccineService.getVaccineById((long) vaccineId);
+        logger.info("Retrieved vaccine: " + retrievedVaccine);
+    }
+
+    @Then("I should get the vaccine details")
+    public void iShouldGetTheVaccineDetails() {
+        Assert.assertFalse(retrievedVaccine.isEmpty());
+        boolean idMatch = retrievedVaccine.stream()
+                .anyMatch(vaccine -> vaccine.getVaccineId().equals(this.vaccineId));
+        Assert.assertTrue(idMatch);
+    }
+
+
     @Given("the system has a record of vaccines administered to a child")
-    public void theSystemHasARecordOfVaccinesAdministeredToAChild() {
-        logger.info("Scenario: Retrieve list of administered vaccines for a child - Step: The system has a record of vaccines administered to a child");
-        request = RestAssured.given();
+    public void theSystemHasARecordOfVaccinesAdministeredToAChildUsingTheServiceLayer() {
+
+        User user = new User();
+        user.setEmailAddress("testuser@example.com");
+        user.setPassword("password123");
+
+
+        user = userRepository.save(user);
+
+        Child child = new Child();
+        child.setName("Boba Fett");
+        child.setDateOfBirth(LocalDate.now());
+        child.setUser(user);
+
+        logger.info("Created child: " + child);
+
+        child = childRepository.save(child);
+
+        if (child.getId() == null) {
+            fail("Child ID not generated!");
+        }
+
+        administeredVaccines = new ArrayList<>();
+
+        Vaccine vaccine1 = new Vaccine(null, "Vaccine 1", 2, LocalDate.now());
+        vaccineService.addVaccine(vaccine1, child);
+        administeredVaccines.add(vaccine1);
+
+        Vaccine vaccine2 = new Vaccine(null, "Vaccine 2", 0, LocalDate.now());
+        vaccineService.addVaccine(vaccine2, child);
+        administeredVaccines.add(vaccine2);
+
+        Vaccine vaccine3 = new Vaccine(null, "Vaccine 3", 4, LocalDate.now());
+        vaccineService.addVaccine(vaccine3, child);
+        administeredVaccines.add(vaccine3);
+
+
+        logger.info("Created administered vaccines: " + child.getAdministeredVaccines());
     }
 
-    /**
-     * Sends a request to fetch the list of administered vaccines for the specified child.
-     */
+
+
     @When("a request is made to fetch the list of administered vaccines for the child")
     public void aRequestIsMadeToFetchTheListOfAdministeredVaccinesForTheChild() {
-        logger.info("Scenario: Retrieve list of administered vaccines for a child - Step: A request is made to fetch the list of administered vaccines for the child");
-        try {
-            response = request.get(childVaccinesEndpoint);
-            logger.info("Received response with status code: " + response.statusCode());
-        } catch (Exception e) {
-            logger.severe("Error fetching list of administered vaccines for the child: " + e.getMessage());
-        }
+        logger.info("Fetching administered vaccines for child: " + child);
+        Child child = childRepository.findById(1L).orElse(null);
+        fetchedAdministeredVaccines = vaccineRepository.findByChild(child.getId());
+        logger.info("Fetched administered vaccines: " + fetchedAdministeredVaccines);
     }
 
-    /**
-     * Validates the response to ensure the list of administered vaccines is returned.
-     */
     @Then("the system should return the list of vaccines administered to the child")
     public void theSystemShouldReturnTheListOfVaccinesAdministeredToTheChild() {
-        logger.info("Scenario: Retrieve list of administered vaccines for a child - Step: The system should return the list of vaccines administered to the child");
-        response.then().statusCode(200);
-    }
+        assertEquals(administeredVaccines.size(), fetchedAdministeredVaccines.size());
 
+        for (int i = 0; i < administeredVaccines.size(); i++) {
+            Vaccine expectedVaccine = administeredVaccines.get(i);
+            Vaccine fetchedVaccine = fetchedAdministeredVaccines.get(i);
 
-    /**
-     * Ensures that the system has a record of vaccines to be administered to a specified child.
-     */
-    // Scenario: Retrieve list of remaining vaccines for a child
-    @Given("the system has a record of vaccines to be administered to a child")
-    public void theSystemHasARecordOfVaccinesToBeAdministeredToAChild() {
-        logger.info("Scenario: Retrieve list of remaining vaccines for a child - Step: The system has a record of vaccines to be administered to a child");
-        request = RestAssured.given();
-    }
-
-    /**
-     * Sends a request to fetch the list of remaining vaccines for the specified child.
-     */
-    @When("a request is made to fetch the list of remaining vaccines for the child")
-    public void aRequestIsMadeToFetchTheListOfRemainingVaccinesForTheChild() {
-        logger.info("Scenario: Retrieve list of remaining vaccines for a child - Step: A request is made to fetch the list of remaining vaccines for the child");
-        try {
-            response = request.get(remainingVaccinesEndpoint);
-            logger.info("Received response with status code: " + response.statusCode());
-        } catch (Exception e) {
-            logger.severe("Error fetching list of remaining vaccines for the child: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Validates the response to ensure the list of remaining vaccines is returned.
-     */
-    @Then("the system should return the list of vaccines yet to be administered to the child")
-    public void theSystemShouldReturnTheListOfVaccinesYetToBeAdministeredToTheChild() {
-        logger.info("Scenario: Retrieve list of remaining vaccines for a child - Step: The system should return the list of vaccines yet to be administered to the child");
-        response.then().statusCode(200);
-    }
-
-
-    /**
-     * Triggers the specified action for a specified vaccine on a specified date.
-     * @param action The action to be triggered.
-     * @param vaccine The vaccine the action is being triggered for.
-     * @param date The date the action is triggered.
-     */
-    // Scenario: Update a vaccine as administered in the database
-    @When("the {string} action is triggered for {string} vaccine with a date of {string}")
-    public void theActionIsTriggeredForVaccineWithADateOf(String action, String vaccine, String date) {
-        logger.info("Scenario: Update a vaccine as administered in the database - Step: The " + action + " action is triggered for " + vaccine + " vaccine with a date of " + date);
-        try {
-            request = RestAssured.given().param("action", action).param("date", date);
-            response = request.put(specificChildVaccineEndpoint);
-            if (response != null) {
-                logger.info("Received response with status code: " + response.statusCode());
-            } else {
-                logger.warning("No response received from the server.");
-            }
-        } catch (Exception e) {
-            logger.severe("An error occurred while triggering action for the vaccine: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Validates the vaccine status and administered date are updated as expected.
-     * @param vaccineName The name of the vaccine.
-     * @param status The updated status of the vaccine.
-     * @param date The date the vaccine was administered.
-     */
-    @Then("{string} status should be updated to {string} with the date {string}")
-    public void statusShouldBeUpdatedToWithTheDate(String vaccineName, String status, String date) {
-        logger.info("Scenario: Update a vaccine as administered in the database - Step: That vaccine's status should be updated to " + status + " with the date " + date);
-        try {
-            response.then().statusCode(200)
-                    .body("status", equalTo(status))
-                    .body("vaccineName", equalTo(vaccineName))
-                    .body("administeredDate", equalTo(date));
-        } catch (Exception e) {
-            logger.severe("An error occurred while updating vaccine status: " + e.getMessage());
+            assertEquals(expectedVaccine.getName(), fetchedVaccine.getName());
+            assertEquals(expectedVaccine.getDateAdministered(), fetchedVaccine.getDateAdministered());
+            assertEquals(expectedVaccine.getChild().getId(), fetchedVaccine.getChild().getId());
         }
     }
 }

12218ab2053c59850bc27e9abb7f6b2e7235bace
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 12:27:02 -0400

    Add JavaDoc to InformationNotFoundException

--- src/main/java/com/example/babyinsightbackend/exception/InformationNotFoundException.java
+++ src/main/java/com/example/babyinsightbackend/exception/InformationNotFoundException.java
@@ -3,6 +3,11 @@ package com.example.babyinsightbackend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+/**
+ * Exception thrown when the information is not found in the system.
+ *
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException {
     public InformationNotFoundException(String message) {

bf20d1ace7f2d575811a178f3470d127df63e5da
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 12:26:48 -0400

    Add JavaDoc to InformationExistException

--- src/main/java/com/example/babyinsightbackend/exception/InformationExistException.java
+++ src/main/java/com/example/babyinsightbackend/exception/InformationExistException.java
@@ -3,6 +3,9 @@ package com.example.babyinsightbackend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when the information is not found in the system.
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException {
     public InformationExistException(String message) { super(message); }

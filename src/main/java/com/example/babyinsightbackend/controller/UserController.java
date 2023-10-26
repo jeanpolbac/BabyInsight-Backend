3ab98547a8f5e63689364e89b987d3d87aa309e2
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 00:25:13 -0400

    Refactor controller methods to use Map for standardized JSON responses

--- src/main/java/com/example/babyinsightbackend/controller/UserController.java
+++ src/main/java/com/example/babyinsightbackend/controller/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -46,19 +48,32 @@ public class UserController {
      * @return ResponseEntity with a success message and HTTP status 201 if registration is successful,
      * or a message indicating that the email already exists with HTTP status 200 if the email is already registered.
      */
+//    @PostMapping(path = "/register/")
+//    public ResponseEntity<?> createUser(@RequestBody User userObject) {
+//        try {
+//            User newUser = userService.createUser(userObject);
+//            logger.info("User successfully created: " + newUser.getEmailAddress());
+//            return new ResponseEntity<>("User successfully created.", HttpStatus.CREATED);
+//        } catch (InformationExistException e) {
+//            logger.warning("Email already exists: " + userObject.getEmailAddress());
+//            return new ResponseEntity<>("Email already exists.", HttpStatus.CONFLICT);
+//        }
+//    }
+
     @PostMapping(path = "/register/")
     public ResponseEntity<?> createUser(@RequestBody User userObject) {
+        Map<String, String> response = new HashMap<>();
         try {
             User newUser = userService.createUser(userObject);
             logger.info("User successfully created: " + newUser.getEmailAddress());
-            return new ResponseEntity<>("User successfully created.", HttpStatus.CREATED);
+            response.put("message", "User successfully created.");
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
         } catch (InformationExistException e) {
             logger.warning("Email already exists: " + userObject.getEmailAddress());
-            return new ResponseEntity<>("Email already exists.", HttpStatus.CONFLICT);
+            response.put("message", "Email already exists.");
+            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
         }
     }
-
-
     /**
      * Endpoint for user login.
      *
@@ -66,26 +81,56 @@ public class UserController {
      * @return ResponseEntity with a JWT token and HTTP status 200 if authentication is successful,
      * or a message indicating authentication failure with HTTP status 401 if authentication fails.
      */
+//    @PostMapping(path = "/login/")
+//    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+//        try {
+//            Optional<String> jwtToken = userService.loginUser(loginRequest);
+//            if (jwtToken.isPresent()) {
+//                logger.info("Authentication successful for user " + loginRequest.getEmailAddress());
+//                return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+//            } else {
+//                logger.warning("Authentication failed for user " + loginRequest.getEmailAddress());
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
+//            }
+//        } catch (InformationNotFoundException e) {
+//            logger.warning("User not found: " + loginRequest.getEmailAddress());
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new LoginResponse("User not found"));
+//        } catch (InvalidPasswordException e) {
+//            logger.warning("Invalid password for user " + loginRequest.getEmailAddress());
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Invalid password"));
+//        } catch (Exception e) {
+//            logger.severe("An unexpected error occurred: " + e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new LoginResponse("An unexpected error occurred"));
+//        }
+//    }
     @PostMapping(path = "/login/")
-    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        Map<String, String> response = new HashMap<>();
         try {
             Optional<String> jwtToken = userService.loginUser(loginRequest);
             if (jwtToken.isPresent()) {
                 logger.info("Authentication successful for user " + loginRequest.getEmailAddress());
-                return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+                response.put("token", jwtToken.get());
+                response.put("message", "Authentication successful.");
+                return new ResponseEntity<>(response, HttpStatus.OK);
             } else {
                 logger.warning("Authentication failed for user " + loginRequest.getEmailAddress());
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
+                response.put("message", "Authentication failed.");
+                return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
             }
         } catch (InformationNotFoundException e) {
             logger.warning("User not found: " + loginRequest.getEmailAddress());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new LoginResponse("User not found"));
+            response.put("message", "User not found.");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         } catch (InvalidPasswordException e) {
             logger.warning("Invalid password for user " + loginRequest.getEmailAddress());
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Invalid password"));
+            response.put("message", "Invalid password.");
+            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
             logger.severe("An unexpected error occurred: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new LoginResponse("An unexpected error occurred"));
+            response.put("message", "An unexpected error occurred.");
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }

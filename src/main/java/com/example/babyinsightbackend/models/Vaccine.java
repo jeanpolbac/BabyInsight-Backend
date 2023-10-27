0fbb27d42fadef441e566ba1995f2929c36e511f
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 05:01:43 -0400

    Add docnotation to ChildRepository, LoginResponse and Vaccine

--- src/main/java/com/example/babyinsightbackend/models/Vaccine.java
+++ src/main/java/com/example/babyinsightbackend/models/Vaccine.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a vaccine entity with details such as vaccine ID, name, recommended age, and date administered.
+ */
 package com.example.babyinsightbackend.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -8,32 +11,47 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "vaccines")
 public class Vaccine {
+    /**
+     * The unique identifier for the vaccine.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long vaccineId;
 
-
-
+    /**
+     * The name of the vaccine.
+     */
     @Column
     private String name;
 
+    /**
+     * The recommended age for administering the vaccine.
+     */
     @Column
     private int recommendedAge;
 
+    /**
+     * The date when the vaccine was administered.
+     */
     @Column
     private LocalDate dateAdministered;
 
-
+    /**
+     * The child associated with the vaccine.
+     */
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "child_id")
     private Child child;
 
-
-
-    public Vaccine() {
-    }
-
+    /**
+     * Constructs a new Vaccine with the provided details.
+     *
+     * @param vaccineId       The unique identifier for the vaccine.
+     * @param name            The name of the vaccine.
+     * @param recommendedAge  The recommended age for administering the vaccine.
+     * @param dateAdministered The date when the vaccine was administered.
+     */
     public Vaccine(Long vaccineId, String name, int recommendedAge, LocalDate dateAdministered) {
         this.vaccineId = vaccineId;
         this.name = name;
@@ -41,44 +59,92 @@ public class Vaccine {
         this.dateAdministered = dateAdministered;
     }
 
-
+    /**
+     * Get the unique identifier for the vaccine.
+     *
+     * @return The vaccine ID.
+     */
     public Long getVaccineId() {
         return vaccineId;
     }
 
+    /**
+     * Set the unique identifier for the vaccine.
+     *
+     * @param vaccineId The vaccine ID to set.
+     */
     public void setVaccineId(Long vaccineId) {
         this.vaccineId = vaccineId;
     }
 
-
+    /**
+     * Get the name of the vaccine.
+     *
+     * @return The vaccine name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name of the vaccine.
+     *
+     * @param name The vaccine name to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the recommended age for administering the vaccine.
+     *
+     * @return The recommended age.
+     */
     public int getRecommendedAge() {
         return recommendedAge;
     }
 
+    /**
+     * Set the recommended age for administering the vaccine.
+     *
+     * @param recommendedAge The recommended age to set.
+     */
     public void setRecommendedAge(String recommendedAge) {
         this.recommendedAge = Integer.parseInt(recommendedAge);
     }
 
+    /**
+     * Get the date when the vaccine was administered.
+     *
+     * @return The date administered.
+     */
     public LocalDate getDateAdministered() {
         return dateAdministered;
     }
 
+    /**
+     * Set the date when the vaccine was administered.
+     *
+     * @param dateAdministered The date administered to set.
+     */
     public void setDateAdministered(LocalDate dateAdministered) {
         this.dateAdministered = dateAdministered;
     }
 
+    /**
+     * Get the child associated with the vaccine.
+     *
+     * @return The child object.
+     */
     public Child getChild() {
         return child;
     }
 
+    /**
+     * Set the child associated with the vaccine.
+     *
+     * @param child The child object to set.
+     */
     public void setChild(Child child) {
         this.child = child;
     }
--- src/main/java/com/example/babyinsightbackend/repository/ChildRepository.java
+++ src/main/java/com/example/babyinsightbackend/repository/ChildRepository.java
@@ -1,3 +1,6 @@
+/**
+ * Repository interface for managing Child entities in the database.
+ */
 package com.example.babyinsightbackend.repository;
 
 import com.example.babyinsightbackend.models.Child;
@@ -10,9 +13,28 @@ import java.util.Optional;
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
 
+    /**
+     * Find all children associated with a specific user.
+     *
+     * @param userId The unique identifier of the user.
+     * @return A list of child entities associated with the user.
+     */
     List<Child> findByUserId(Long userId);
 
+    /**
+     * Find a child by their name.
+     *
+     * @param name The name of the child to search for.
+     * @return An optional child entity with the specified name.
+     */
     Optional<Child> findByName(String name);
 
+    /**
+     * Find a child by their ID and associated user's ID.
+     *
+     * @param childId The unique identifier of the child.
+     * @param userId  The unique identifier of the user associated with the child.
+     * @return The child entity with the specified ID and associated user's ID.
+     */
     Child findByIdAndUserId(Long childId, Long userId);
 }

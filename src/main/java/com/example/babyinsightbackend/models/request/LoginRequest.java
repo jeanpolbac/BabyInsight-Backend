d4ee7cde94a6ff0e4512dfeabcd6f93cab82db0b
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 04:58:02 -0400

    Add docnotation to LoginRequest

--- src/main/java/com/example/babyinsightbackend/models/request/LoginRequest.java
+++ src/main/java/com/example/babyinsightbackend/models/request/LoginRequest.java
@@ -1,15 +1,34 @@
+/**
+ * Represents a request object for user login.
+ */
 package com.example.babyinsightbackend.models.request;
 
 public class LoginRequest {
 
+    /**
+     * The email address of the user attempting to log in.
+     */
     private String emailAddress;
 
+    /**
+     * The password of the user attempting to log in.
+     */
     private String password;
 
+    /**
+     * Get the email address of the user.
+     *
+     * @return The user's email address.
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     * Get the password of the user.
+     *
+     * @return The user's password.
+     */
     public String getPassword() {
         return password;
     }
